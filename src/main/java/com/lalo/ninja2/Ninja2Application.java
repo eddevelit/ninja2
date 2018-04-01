581328a2bcbed60d70e5e3284d41308dd037ae16
eddevelit <edd.develit@gmail.com>
2018-03-31 20:22:02 -0600

    Inicializando proyecto con la configuracion de conexion, JPA Y Hibernate en el archivo application.yml

---
+++ src/main/java/com/lalo/ninja2/Ninja2Application.java
@@ -0,0 +1,12 @@
+package com.lalo.ninja2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ninja2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ninja2Application.class, args);
+	}
+}
