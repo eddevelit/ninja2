c2bba2d82c08af2c363180bd7453e609ef6cf66d
Eduardo Ocampo <edd.develit@gmail.com>
2018-04-10 10:42:49 -0500

    Creando e integrando todas las capas

--- src/main/java/com/lalo/ninja2/service/CourseService.java
+++ src/main/java/com/lalo/ninja2/service/CourseService.java
@@ -1,5 +1,14 @@
 package com.lalo.ninja2.service;
 
+import java.util.List;
+
+import com.lalo.ninja2.entity.Course;
+
 public interface CourseService {
 
+	public abstract List<Course> listAllCourses();
+	public abstract Course addCourse(Course course);
+	public abstract int removeCourse(int id);
+	public abstract Course updateCourse(Course course);
+	
 }
--- src/main/java/com/lalo/ninja2/service/impl/CourseServiceImpl.java
+++ src/main/java/com/lalo/ninja2/service/impl/CourseServiceImpl.java
@@ -1,5 +1,40 @@
 package com.lalo.ninja2.service.impl;
 
-public class CourseServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.lalo.ninja2.entity.Course;
+import com.lalo.ninja2.repository.CourseJpaRepository;
+import com.lalo.ninja2.service.CourseService;
+
+public class CourseServiceImpl implements CourseService{
+	
+	@Autowired
+	@Qualifier("courseJpaRepository")
+	private CourseJpaRepository courseRepository;
+
+	@Override
+	public List<Course> listAllCourses() {
+		return courseRepository.findAll();
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+		return courseRepository.save(course);
+	}
+
+	@Override
+	public int removeCourse(int id) {
+		courseRepository.deleteById(id);
+		return 0;
+	}
+
+	@Override
+	public Course updateCourse(Course course) {
+		return courseRepository.save(course);
+	}
+	
+	
 }
